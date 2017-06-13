f3029ed0bbd85d6ec0a5da53f62504317d5e3408
Joel Bakken <jkbetc@gmail.com>
2017-06-13 16:42:49 -0700

    correct js code to allow user answers to actually populate a suggested tract. Also adds the readme file and some minor css formatting changes

--- js/scripts.js
+++ js/scripts.js
@@ -1,24 +1,28 @@
 $(document).ready(function() {
-      $("#formOne").submit(function(event) {
-            event.preventDefault();
+  $("form").submit(function(event) {
+    event.preventDefault();
 
-var yourName = $("yourName").val();
+    var yourName = $("yourName").val();
 
-var companySize = $("input:radio[name=companysize]:checked").val();
+    var companySize = $("input:radio[name=companysize]:checked").val();
 
-var workPretty = $("input:radio[name=workpretty]:checked").val();
+    var workPretty = $("input:radio[name=workpretty]:checked").val();
 
-var enjoyComputers = $("input:radio[name=enjoycomputers]:checked").val();
+    var enjoyComputers = $("input:radio[name=enjoycomputers]:checked").val();
 
-var mobileApps = $("input:radio[name=mobileapps]:checked").val();
+    var mobileApps = $("input:radio[name=mobileapps]:checked").val();
 
-if ((mobileApps === yes && mobileApps !== no) && enjoyComputers === yes && (companySize === startup || companySize === enterprise) && (workPretty === yes || workpPretty === no)) {
-  return ("you should take the Java course tract." + show("#java"));
-} else if (mobileApps === no && enjoyComputers === yes && (companySize === startup || companySize === enterprise) && (workPretty === yes || workPretty === no)) {
-  return ("you should take the Design and/or ruby course tracts." + $("#whattract").show("#design", "#ruby"));
-} else if (mobileApps === yes && enjoyComputers === yes && (companySize === enterprise || companySize === startup) && (workPretty === yes || workPretty === yes)) {
-  return ("#yourName" + " you should take all of the cource tracts." + show("#design" + "#csharp" + "#java" + "#php" + "#ruby"));
-} else if(enjoyComputers === no); {
-  return ("You probably should not take any of these courses if you don't like computers!");
-}
-//put code to display the results of the form here.
+    if ((mobileApps === "yes" && enjoyComputers === "yes" && companySize === "startup") || (mobileApps === "yes" && enjoyComputers === "yes" && companySize === "enterprise")) {
+    $("#design").show();
+
+      } else if (mobileApps === "no" && enjoyComputers === "yes" && workpretty === "pretty" && companysize === "startup") {
+      $("#csharp").show();
+
+      } else if ((mobileApps === "no" && enjoyComputers === "yes") || (mobileApps === "yes" && enjoyComputers === "yes" && companySize === "enterprise")) {
+        $("#ruby").show();
+
+      } else if (workpretty === "pretty" && enjoycomputers === "no") {
+        $("#php")
+      }  
+  });
+});
---
+++ readme.md
@@ -0,0 +1,33 @@
+# Tract Chooser
+
+#### Helps you decide what classes to take at Epicodus [ver 1.4453 6-13-17]
+
+#### By Joel Bakken
+
+## Description
+
+This program gives the prospective student several questions to choose from and them suggests different tracts to take based on their responses.
+
+## Setup/Installation Requirements
+
+* No setup required, just load index.html into your favorite web browser and follow the on screen instructions.
+
+
+
+## Known Bugs
+
+There are no known bugs at this time.
+
+## Support and contact details
+
+Please contact Joel & jkbetc@gmail.com if you have any questions about this program.
+
+## Technologies Used
+
+Written on a Dell OMEN laptop using Atom and Sublime Text. I used HTML, CSS, JavaScript and Jquery in the making of this program.
+
+### License
+
+*MIT License*
+
+Copyright (c) 2017 **Joel Bakken**
